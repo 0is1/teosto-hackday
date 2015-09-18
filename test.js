23afd68e1d39c957c0396c620b3ae690d70ff278
Janne Saarela <janne.juhani@gmail.com>
2015-09-18 17:02:15 +0300

    Initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+var TeostoApi = require('./TeostoApi');
+
+TeostoApi.getVenueByName('tavastia', function(error, response, body) {
+ if (error) console.log(error);
+ console.log('status: ', response.statusCode);
+ console.log(body);
+});
+
+TeostoApi.getEventById('80a4083b53', function(error, response, body) {
+  if (error) console.log(error);
+  console.log('status: ', response.statusCode);
+  console.log(body);
+});
+
+TeostoApi.getDatesByMonth(9, 1, 3, function(error, response, body) {
+  if (error) console.log(error);
+  console.log('status: ', response.statusCode);
+  console.log(body);
+});
